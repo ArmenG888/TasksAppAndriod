f789902818363c104d8d274b8f4736cf762e8786
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-07-24 14:49:04 -0700

    User input on tasks

--- app/src/main/java/com/example/tasksapp/MainActivity.kt
+++ app/src/main/java/com/example/tasksapp/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.tasksapp
 import CustomAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +19,16 @@ class MainActivity : AppCompatActivity() {
         recyclerview.layoutManager = LinearLayoutManager(this)
         val data = ArrayList<ItemsViewModel>()
 
-        for (i in 1..20) {
-            data.add(ItemsViewModel("Task $i" ))
+
+        val addButton = findViewById<Button>(R.id.addButton)
+
+
+        addButton.setOnClickListener {
+            val task = findViewById<EditText>(R.id.taskInput)
+            data.add(ItemsViewModel(task.text.toString()))
+            val adapter = CustomAdapter(data)
+            recyclerview.adapter = adapter
+            task.setText("")
         }
-
-        val adapter = CustomAdapter(data)
-
-        recyclerview.adapter = adapter
     }
 }
