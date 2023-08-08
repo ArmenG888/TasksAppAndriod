1c1b428d1a8bcd97a648bb607b2206623c19708a
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-08-08 15:53:16 -0700

    enter-button on the keyboard

--- app/src/main/java/com/example/tasksapp/MainActivity.kt
+++ app/src/main/java/com/example/tasksapp/MainActivity.kt
@@ -4,6 +4,8 @@ import CustomAdapter
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
@@ -46,5 +48,23 @@ class MainActivity : AppCompatActivity() {
         taskInput.addTextChangedListener {
             addButton.isEnabled = taskInput.text.toString() != ""
         }
+        taskInput.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                (event?.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER)) {
+                // The same action as your addButton
+                data.add(ItemsViewModel(taskInput.text.toString()))
+                adapter.notifyDataSetChanged()
+                taskInput.setText("")
+
+                val tasksStringUpdated = data.joinToString(",") { it.text }
+                editor.putString("tasks", tasksStringUpdated)
+                editor.apply()
+
+                true // consume the event
+            } else {
+                false // let other events be handled by the framework
+            }
+        }
+
     }
 }
